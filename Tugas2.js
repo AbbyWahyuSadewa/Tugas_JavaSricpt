9a271e96e7872c1d50ff8190352a3e4026c28933
Abby <125106565+AbbyWahyuSadewa@users.noreply.github.com>
2023-09-07 08:02:14 +0700

    Add files via upload

---
+++ Tugas2.js
@@ -0,0 +1,25 @@
+var number = 43;
+
+var string = "Abby Wahyu Sadewa";
+
+var boolean = 150;
+
+if (boolean > 170) {
+  true;
+} else  {
+  false;
+}
+
+var nul = "ada";
+nul = null;
+
+var ang = [2, 3, 4, 5, 6, 7, 8];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nul);
+console.log(ang[2]); //angka yang di dalam kurung tersbut untuk memanggil angka yang tersedia pada var array, yang di mulai dari angka 2 pada array tersebut
+//misal seperti angka 2 dalam kurung console terbeut, dia akan memanggil angka ke 2 dari var array kita, bila kita jalankan hasilnya akan muncul angka 4 karena angka ke 2 dari var array kita adalah 4
+
+// JANGAN LUPA "HAPPY CODING :)"
---
+++ Tugas3.js
@@ -0,0 +1,9 @@
+function pangkat(x) {
+  console.log("nilai sebelum  di pangkat " + x);
+  y = x ** 7;
+  return y;
+}
+
+let nilai = 6;
+nilai = pangkat(nilai);
+console.log("hasil sesudah di pangkat " + y)
